c01e10b916766b43b244a1cd3a69aa2e2205a1e1
Tarun Batra <tarun.batra00@gmail.com>
2021-01-24 01:49:09 +0100

    Add analytics

---
+++ public/analytics.js
@@ -0,0 +1,11 @@
+(function(c,l,a,r,i,t,y){
+	c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+	t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+	y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "53gkip7xyg");
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-1L564V72K9');
